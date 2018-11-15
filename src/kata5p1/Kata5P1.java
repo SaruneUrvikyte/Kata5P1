9d6a47aa6a80ef621ebcd6feaf11bfa58959ddb9
SaruneUrvikyte <sarune.urvikyte@gmail.com>
2018-11-15 11:18:50 +0000

    Kata5P1 v2

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,9 +1,5 @@
 package kata5p1;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Kata5P1 {
 
     public static void main(String[] args) {
